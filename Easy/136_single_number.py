130196d619de21528b3e98b0ac49db72eec1f8af
mirdula_rajini <mirdula.2810@gmail.com>
2025-09-20 22:53:36 +0530

    136_single_number

---
+++ Easy/136_single_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        index = 0
+        for num in nums:
+            index ^= num
+        return index
+            
+        
